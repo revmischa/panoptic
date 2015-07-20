14914ecfc4e4ccd890d47903c3f532b760e879d2
Devin Austin <devin.austin@gmail.com>
2015-07-19 18:26:55 -0600

    added testify assertions

--- gst_test.go
+++ gst_test.go
@@ -2,6 +2,7 @@ package panoptic
 
 import (
 	"testing"
+        "github.com/stretchr/testify/assert"
 	"fmt"
 )
 
@@ -12,11 +13,10 @@ func TestMain(m *testing.M) {
 */
 
 func TestGSTVersion(t *testing.T) {
+        assert := assert.New(t)
 	major, minor, micro, nano := GSTVersion()
 
-	if (major != 1) {
-		t.Error("Major version should be 1, got: " + string(major))
-	}
+        assert.Equal(major, 1, "Major version should be 1, got: " + string(major))
 
 	fmt.Printf("gstreamer version reported: %v.%v.%v.%v\n", major, minor, micro, nano)
 }
